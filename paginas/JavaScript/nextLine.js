37d07e28d0c7903cf2c4f987a9195ff6c0d1e45c
XamSav <samuel81@hotmail.es>
2023-01-04 19:33:50 +0100

    Acabado

--- paginas/JavaScript/nextLine.js
+++ paginas/JavaScript/nextLine.js
@@ -1,11 +1,32 @@
-let scriptArduino = ["void setup(){", "Serial.Begin(9600);","}","void loop(){","Serial.Write('Hola');","}"];
-let scriptUnity = ["using System.Collections;", "using System.Collections.Generic;", "using UnityEngine;",
-"using System.IO.Ports;", "using System;", "public class ArduinoController : MonoBehaviour{",
-"SerialPort serialPort = new SerialPort('COM4', 9600);", "void Start(){", "serialPort.Open();",
-"serialPort.ReadTimeout = 100;","}","void Update(){","if (serialPort.IsOpen)","Debug.Log(serialPort.ReadLine());"];
+let scriptArduino = [
+    "bool activeHola = false;",
+    "void setup(){",
+    "\tSerial.Begin(9600);",
+    "}",
+    "void loop(){",
+    "\tSerial.Write(activeHola == true ? 'Hola' : 'Adios');",
+    "\tdelay(1000);",
+    "\tactiveHola = !activeHola;",
+    "}"];
+let scriptUnity = [
+    "using System.Collections;", "using System.Collections.Generic;", "using UnityEngine;","using System.IO.Ports;", "using System;", 
+    "public class ArduinoController : MonoBehaviour{",
+    "\tSerialPort serialPort = new SerialPort('COM4', 9600);",
+    "\tvoid Start(){",
+    "\t\tGameObject.Find('Square').SetActive(false);",
+    "\t\tserialPort.Open();",
+    "\t\tserialPort.ReadTimeout = 100;","\t}",
+    "\tvoid Update(){",
+    "\t\tif (serialPort.IsOpen){",
+    "\t\t\tif(serialPort.ReadLine() == 'Hola')",
+    "\t\t\t\tGameObject.Find('Square').SetActive(true);",
+    "\t\t\telse",
+    "\t\t\t\tGameObject.Find('Square').SetActive(false);",
+    "\t\t}","}"];
+let imagenesUnity = [""]
 var caja;
 var index = 0;
-
+let indexImg = 2;
 function nextLineArduino(){
     let txt = "";
     for(let a = 0;  a <= index; a++){
@@ -19,10 +40,25 @@ function nextLineArduino(){
     }
 }
 function nextLineUnityEscena(){
-
+        document.getElementById("caja").src="../img/PreparandoUnity"+indexImg+".png"
+        
+        if(indexImg >= 5){
+            document.getElementById("boton").value = "Siguiente Pagina";
+            document.getElementById("boton").onclick = nextPage;
+        }
+        indexImg++;
 }
 function nextLineUnityScript(){
-
+    let txt = "";
+    for(let a = 0;  a <= index; a++){
+        txt += scriptUnity[a]+"\r\n";
+    }
+    document.getElementById("caja").value = txt;
+    index++;
+    if(index >= scriptUnity.length){
+        document.getElementById("boton").value = "Siguiente Pagina";
+        document.getElementById("boton").onclick = nextPage;
+    }
 }
 function nextPage(){
     location.href = document.getElementById("boton").name;
