92983366a0aebdc4bc0162fb2c9e3f6583b05815
Kiznaiver27 <samuel81@hotmail.es>
2023-01-11 13:42:50 +0100

    .

--- paginas/JavaScript/nextLine.js
+++ paginas/JavaScript/nextLine.js
@@ -1,3 +1,8 @@
+
+/*
+Primero estan los bloques de codigo que se muestran al usuario
+*/
+
 let scriptArduino = [
     "bool activeHola = false;",
     "void setup(){",
@@ -23,10 +28,16 @@ let scriptUnity = [
     "\t\t\telse",
     "\t\t\t\tGameObject.Find('Square').SetActive(false);",
     "\t\t}","}"];
+
+    /*Para las imagenes utilizo una lista que luego agregare con ../Preparando...+index*/
 let imagenesUnity = [""]
 var caja;
 var index = 0;
 let indexImg = 2;
+
+/*Esta funcion solo la llama el html de ScriptArduino
+Se encarga de mostrar al usuario la siguiente linea y de cambiar el valor del boton para enviar
+al usuario a la siguiente pagina */
 function nextLineArduino(){
     let txt = "";
     for(let a = 0;  a <= index; a++){
@@ -39,6 +50,9 @@ function nextLineArduino(){
         document.getElementById("boton").onclick = nextPage;
     }
 }
+/*Esta funcion solo la llama el html de PreparandoEscenaUnity
+Se encarga de mostrar al usuario la siguiente imagen y de cambiar el valor del boton para enviar
+al usuario a la siguiente pagina */
 function nextLineUnityEscena(){
         document.getElementById("caja").src="../img/PreparandoUnity"+indexImg+".png"
         
@@ -48,6 +62,9 @@ function nextLineUnityEscena(){
         }
         indexImg++;
 }
+/*Esta funcion solo la llama el html de ScriptUnity
+Se encarga de mostrar al usuario la siguiente linea y de cambiar el valor del boton para enviar
+al usuario a la siguiente pagina */
 function nextLineUnityScript(){
     let txt = "";
     for(let a = 0;  a <= index; a++){
@@ -60,6 +77,7 @@ function nextLineUnityScript(){
         document.getElementById("boton").onclick = nextPage;
     }
 }
+/*Cuando esta funcion es llamada, envia al usuario a la pagina descrita en el name del propio boton */
 function nextPage(){
     location.href = document.getElementById("boton").name;
 }
